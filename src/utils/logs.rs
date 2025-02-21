061a07af34aeb0bd5373aef803a2136694b70280
AlbanDAVID <alban.dav@live.fr>
2025-02-21 18:08:39 +0100

    set up log .config 1

--- src/utils/logs.rs
+++ src/utils/logs.rs
@@ -2,14 +2,19 @@ use log::{info, warn, error, LevelFilter};
 use fern::Dispatch;
 use chrono::Local;
 use std::fs::OpenOptions;
+use std::path::PathBuf;
+
 
 pub fn setup_logs() -> Result<(), fern::InitError> {
+    let mut log_path = dirs::config_dir().unwrap_or_else(|| PathBuf::from("."));
+    log_path.push("toutui/toutui.log");
+
     // Create or append into the file
     let log_file = OpenOptions::new()
         .create(true)
         .write(true)
         .append(true)
-        .open("toutui.log") // path and name
+        .open(log_path) // path and name
         .unwrap();
 
     Dispatch::new()
