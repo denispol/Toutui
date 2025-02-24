1b198b77b6e3df35a0a94a5c798700d0a07144b6
AlbanDAVID <alban.dav@live.fr>
2025-02-24 21:54:11 +0100

    update changelog

--- src/utils/changelog.rs
+++ src/utils/changelog.rs
@@ -17,7 +17,7 @@ let changelog_01 = format!(
          ####\n"
     );
 let changelog_02 = format!(
-        "Changelog Toutui v{} (02/24/2025) \n\
+        "Changelog Toutui v0.1.1-beta (02/24/2025) \n\
          Fixed:\n\
          \n\
          - App crash (out of bounds) when API send empty values.
@@ -29,9 +29,24 @@ let changelog_02 = format!(
          \n\
          Enjoy and be toutui!\n
          ####\n",
+    );
+let changelog_03 = format!(
+        "Changelog Toutui v{} (02/24/2025) \n\
+         Fixed:\n\
+         \n\
+         - Partially fixed, becsause not optimal: bug_id: 9bacac Sync: If you open VLC to listen X, close VLC and quickly open VLC again to listen Y: X will still be sync — according to Y (normally, only Y has to be sync in this case).
+
+         \n\
+         Changed:\n\
+         \n\
+         No change.
+         \n\
+         Enjoy and be toutui!\n
+         ####\n",
          VERSION
     );
 
+    changelog.push_str(&changelog_03); 
     changelog.push_str(&changelog_02); 
     changelog.push_str(&changelog_01); 
 
