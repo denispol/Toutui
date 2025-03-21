125c80caf0724afef57a5e7b05913169ce1f3dd1
AlbanDAVID <alban.dav@live.fr>
2025-03-21 18:40:52 +0100

    update player progress 1

--- src/player/integrated/player_info.rs
+++ src/player/integrated/player_info.rs
@@ -1,6 +1,20 @@
 use log::info;
 use crate::db::crud::*;
 
+fn format_time(seconds: u32) -> String {
+    let hours = seconds / 3600;
+    let minutes = (seconds % 3600) / 60;
+    let secs = seconds % 60;
+
+    if hours > 0 {
+        format!("{}:{:02}:{:02}", hours, minutes, secs)
+    } else if minutes > 0 {
+        format!("{}:{:02}", minutes, secs)
+    } else {
+        format!("0:{}", secs)
+    }
+}
+
 pub fn player_info(username: &str) -> Vec<String> {
     let mut player_info = Vec::new();
 
@@ -8,9 +22,20 @@ pub fn player_info(username: &str) -> Vec<String> {
         Ok(Some(session)) => {
             player_info.push(session.title);
             player_info.push(session.author);
-            player_info.push(session.current_time.to_string());
-            player_info.push(session.duration);
-            player_info.push(session.elapsed_time.to_string());
+            player_info.push(format_time(session.current_time));
+
+            let speed_rate_str = get_speed_rate(username);
+            
+            let speed_rate: f32 = speed_rate_str.parse().unwrap_or(1.0);
+
+            let original_duration = session.duration.parse::<u32>().unwrap_or(0);
+            
+            let adjusted_duration = (original_duration as f32 / speed_rate) as u32;
+            player_info.push(format_time(adjusted_duration)); 
+
+            let remaining_time = adjusted_duration.saturating_sub(session.current_time);
+            player_info.push(format_time(session.elapsed_time));
+            player_info.push(format_time(remaining_time)); 
         }
         Ok(None) => {
             player_info.push(format!("N/A"));
@@ -21,7 +46,8 @@ pub fn player_info(username: &str) -> Vec<String> {
         }
     }
 
-    player_info.push(get_speed_rate(username).to_string());
+    player_info.push(get_speed_rate(username));
 
     player_info
 }
+
