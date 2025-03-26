4d2f7571d307e3e014ce380930a5b292e18147f7
AlbanDAVID <alban.dav@live.fr>
2025-03-26 09:47:02 +0100

    macos path for logs

--- src/utils/logs.rs
+++ src/utils/logs.rs
@@ -2,16 +2,14 @@ use log::LevelFilter;
 use fern::Dispatch;
 use chrono::Local;
 use std::fs::OpenOptions;
-use std::path::PathBuf;
-
 
 pub fn setup_logs() -> Result<(), fern::InitError> {
     let mut log_path = if cfg!(target_os = "macos") {
-    let mut path = dirs::home_dir().unwrap_or_else(|| PathBuf::from("."));
-    path.push(".config");
+    let mut path = dirs::home_dir().expect("Unable to find the user's home directory");
+    path.push("Library/Application Support");
     path
 } else {
-    dirs::config_dir().unwrap_or_else(|| PathBuf::from("."))
+    dirs::config_dir().expect("Unable to find the .config directory")
 };
 log_path.push("toutui/toutui.log");
 
